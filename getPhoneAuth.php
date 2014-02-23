f34e22df2315c72eb9a62b2631e1d0d55b282d30
Tijs Zwinkels - TinkerTank <tijs@tinkertank.eu>
2014-02-23 16:48:40 +0000

    Working server-side logic.

--- getPhoneAuth.php
+++ getPhoneAuth.php
@@ -3,29 +3,28 @@
 	$SQLPASS = "S4sHmqxqIU";
 
 	$db = new mysqli("localhost", $SQLUSER, $SQLPASS, "bedien01_main");
-	//$result = $mysqli->query("SELECT * ");
 
 	// For now, expect auth as base64 encoded username:server:password 
-	$username = ($_POST['username']) ? $_POST['username'] : "tijs";
-	$server = ($_POST['server']) ? $_POST['server'] : "yyy";
-	$auth = ($_POST['auth']) ? $_POST['auth'] : "x";
+	$username = ($_POST['username']) ? $_POST['username'] : "";
+	$server = ($_POST['server']) ? $_POST['server'] : "";
+	$auth = ($_POST['auth']) ? $_POST['auth'] : "";
 
-	echo ("Query input: $username, $server, $auth");
+	//echo ("Query input: $username, $server, $auth");
 
 	$statement = $db->prepare("SELECT phoneIp, phoneUser, phonePass, username, server, auth FROM users WHERE username = ? AND server = ? AND auth = ?");
-
 	$statement->bind_param("sss", $username, $server, $auth);
-
 	$statement->execute();
-	//$result = $statement->get_result();
 	$statement->bind_result($phoneIp, $phoneUser, $phonePass, $username, $server, $auth);
-
 	$statement->fetch();
 
-	echo "Results: $phoneIp, $phoneUser, $phonePass";
+	if (!$phoneIp) {
+		header('HTTP/1.0 403 Forbidden');
+	} else {
+		$clientData = array('phoneIp' => $phoneIp, 'phoneUser' => $phoneUser, 'phonePass' => $phonePass);
+		header('Content-type: application/json');
+		echo json_encode($clientData);
+	}
 
 	$statement->close();
-
-	//phpinfo();
 
 ?>
